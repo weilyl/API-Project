f080ce8e5636a6089374a225116f981b5cb325bc
ukhan12 <ubkhan3@gmail.com>
2022-03-01 15:24:22 -0500

    fix dom

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener(DOMContentLoded, () => {
+window.addEventListener('DOMContentLoaded', () => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=chicken`)
     .then(res=>res.json())
     .then(dataa=> console.log(dataa))
