a61bcddcf63c8297663299bb6ce05a3985be6586
Khalia Parris <khaliaparris01@gmail.com>
2022-03-01 15:21:06 -0500

    added fecth api

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hello')
+document.addEventListener(DOMContentLoded, () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=chicken`)
+    .then(res=>res.json())
+    .then(dataa=> console.log(dataa))
+})
